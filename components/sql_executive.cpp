83139e31d8a6efb428e34d6c2b8bfbcab5ef0fb1
createsejin <createsejin@gmail.com>
2024-02-08 15:45:58 +0900

    24-02-08_003

--- components/sql_executive.cpp
+++ components/sql_executive.cpp
@@ -40,12 +40,12 @@ namespace sql_executive
         return rc;
     }
 
-    auto prepare_stmt(const char* sql) -> sqlite3_stmt* {
+    auto prepare_stmt(sqlite3* db, const char* sql) -> sqlite3_stmt* {
         sqlite3_stmt* stmt;
-        const auto rc = sqlite3_prepare_v2(mecro_data, sql, -1, &stmt, nullptr);
+        const auto rc = sqlite3_prepare_v2(db, sql, -1, &stmt, nullptr);
         if (rc != SQLITE_OK) {
-            std::cerr << "Failed to prepare statement \u25A1" << std::endl;
-            sqlite3_close_v2(mecro_data);
+            std::cerr << "Failed to prepare statement: "<< sqlite3_errmsg(db) <<" \u25A1" << std::endl;
+            sqlite3_close_v2(db);
             return nullptr;
         }
         return stmt;
@@ -53,7 +53,7 @@ namespace sql_executive
     int step_stmt(sqlite3* db, sqlite3_stmt* stmt) {
         const auto rc = sqlite3_step(stmt);
         if (rc != SQLITE_DONE) {
-            std::cerr << "Failed to execute SQL statement \u25A1" << std::endl;
+            std::cerr << "Failed to execute SQL statement: "<< sqlite3_errmsg(db) <<" \u25A1" << std::endl;
             sqlite3_finalize(stmt);
             sqlite3_close_v2(db);
             return rc;
@@ -63,7 +63,7 @@ namespace sql_executive
     int reset_stmt(sqlite3* db, sqlite3_stmt* stmt) {
         const auto rc = sqlite3_reset(stmt);
         if (rc != SQLITE_OK) {
-            std::cerr << "Failed to reset the statement \u25A1" << std::endl;
+            std::cerr << "Failed to reset the statement: "<< sqlite3_errmsg(db) <<" \u25A1" << std::endl;
             sqlite3_finalize(stmt);
             sqlite3_close_v2(db);
             return rc;
@@ -74,7 +74,7 @@ namespace sql_executive
     int sql_bind_int(sqlite3* db, sqlite3_stmt* stmt, const int pos, const int value) {
         const auto rc = sqlite3_bind_int(stmt, pos, value);
         if (rc != SQLITE_OK) {
-            std::cerr << "Failed to bind key value \u25A1" << std::endl;
+            std::cerr << "Failed to bind key value: " << sqlite3_errmsg(db) << " \u25A1" << std::endl;
             sqlite3_finalize(stmt);
             sqlite3_close_v2(db);
             return rc;
@@ -84,7 +84,7 @@ namespace sql_executive
     int sql_bind_text(sqlite3* db, sqlite3_stmt* stmt, const int pos, const char* text) {
         const auto rc = sqlite3_bind_text(stmt, pos, text, -1, SQLITE_STATIC);
         if (rc != SQLITE_OK) {
-            std::cerr << "Failed to bind key name \u25A1" << std::endl;
+            std::cerr << "Failed to bind key name: " << sqlite3_errmsg(db) << " \u25A1" << std::endl;
             sqlite3_finalize(stmt);
             sqlite3_close_v2(db);
             return rc;
@@ -140,7 +140,7 @@ namespace sql_executive
         begin_transaction(mecro_data);
 
         const auto* sql = "INSERT INTO key_code(key_value, key_name) VALUES(?, ?);";
-        sqlite3_stmt* stmt = prepare_stmt(sql); if (stmt == nullptr) return;
+        sqlite3_stmt* stmt = prepare_stmt(mecro_data, sql); if (stmt == nullptr) return;
 
         key_patterns::KeyCodeManager& key_code_manager = key_patterns::KeyCodeManager::getInstance();
         const auto& key_code_map = key_code_manager.get_keyCode_name_map();
@@ -164,7 +164,7 @@ namespace sql_executive
             cout << "Result of the table key_code:" << endl;
         // Execute SELECT * FROM key_code;
         sql = "SELECT * FROM key_code;";
-        stmt = prepare_stmt(sql); if (stmt == nullptr) return;
+        stmt = prepare_stmt(mecro_data, sql); if (stmt == nullptr) return;
         cout << "pk | key_value | key_name" << endl;
         while (sqlite3_step(stmt) == SQLITE_ROW) {
             cout << sqlite3_column_int(stmt, 0)<< " | " << sqlite3_column_int(stmt, 1) << " | "
@@ -176,108 +176,6 @@ namespace sql_executive
         // Ask user to commit or rollback
         commit_or_rollback(mecro_data);
 
-        sqlite3_close_v2(mecro_data);
-    }
-
-    void insert_key_code_oldv() {
-        if (open_mecro_data() != 0) return;
-
-        // BEGIN TRANSACTION
-        char* errmsg;
-        if (sqlite3_exec(mecro_data, "BEGIN TRANSACTION;", nullptr, nullptr, &errmsg) != SQLITE_OK) {
-            std::cerr << "Failed to begin transaction \u25A1" << std::endl;
-            sqlite3_close_v2(mecro_data);
-            return;
-        }
-
-        const auto* sql = "INSERT INTO key_code(key_value, key_name) VALUES(?, ?);";
-        sqlite3_stmt* stmt;
-        if (sqlite3_prepare_v2(mecro_data, sql, -1, &stmt, nullptr) != SQLITE_OK) {
-            std::cerr << "Failed to prepare statement \u25A1" << std::endl;
-            sqlite3_close_v2(mecro_data);
-            return;
-        }
-        key_patterns::KeyCodeManager& key_code_manager = key_patterns::KeyCodeManager::getInstance();
-        const auto& key_code_map = key_code_manager.get_keyCode_name_map();
-        for (const auto&[key, value] : key_code_map) {
-            // Bind the key_value parameter
-            if (sqlite3_bind_int(stmt, 1, static_cast<int>(key)) != SQLITE_OK) {
-                std::cerr << "Failed to bind key value \u25A1" << std::endl;
-                sqlite3_finalize(stmt);
-                sqlite3_close_v2(mecro_data);
-                return;
-            }
-            // Bind the key_name parameter
-            if (sqlite3_bind_text(stmt, 2, value.c_str(), -1, SQLITE_STATIC) != SQLITE_OK) {
-                std::cerr << "Failed to bind key name \u25A1" << std::endl;
-                sqlite3_finalize(stmt);
-                sqlite3_close_v2(mecro_data);
-                return;
-            }
-            // Execute the statement
-            if (sqlite3_step(stmt) != SQLITE_DONE) {
-                std::cerr << "Failed to execute SQL statement: " << sqlite3_errmsg(mecro_data)
-                << " \u25A1" << std::endl;
-                sqlite3_finalize(stmt);
-                sqlite3_close_v2(mecro_data);
-                return;
-            }
-            // Reset the statement for the next iteration
-            if (sqlite3_reset(stmt) != SQLITE_OK) {
-                std::cerr << "Failed to reset the statement \u25A1" << std::endl;
-                sqlite3_finalize(stmt);
-                sqlite3_close_v2(mecro_data);
-                return;
-            }
-        }
-        // Finalize the statement
-        sqlite3_finalize(stmt);
-        cout << "Inserted key codes successfully \u25A0" << endl << endl;
-
-        cout << "Result of the table key_code:" << endl;
-        // Execute SELECT * FROM key_code;
-        sql = "SELECT * FROM key_code;";
-        if (sqlite3_prepare_v2(mecro_data, sql, -1, &stmt, nullptr) != SQLITE_OK) {
-            std::cerr << "Failed to prepare statement \u25A1" << std::endl;
-            sqlite3_close_v2(mecro_data);
-            return;
-        }
-        cout << "pk | key_value | key_name" << endl;
-        while (sqlite3_step(stmt) == SQLITE_ROW) {
-            cout << sqlite3_column_int(stmt, 0)<< " | " << sqlite3_column_int(stmt, 1) << " | "
-            << sqlite3_column_text(stmt, 2) << endl;
-        }
-        // Finalize the statement
-        sqlite3_finalize(stmt);
-
-        // Ask user to commit or rollback
-        while (true) {
-            string input;
-            cout << "Do you want to commit or rollback? (commit/rollback)" << endl;
-            cout << "cmd> ";
-            std::getline(std::cin, input);
-            const auto pos = input.find("cmd>");
-            if (pos != std::string::npos) {
-                input.erase(pos, 5);
-            }
-            if (input == "commit") {
-                if (sqlite3_exec(mecro_data, "COMMIT;", nullptr, nullptr, &errmsg) != SQLITE_OK) {
-                    std::cerr << "Failed to commit transaction \u25A1" << std::endl;
-                    sqlite3_free(errmsg);
-                }
-                cout << "Committed transaction \u25A0" << endl;
-                break;
-            }
-            if (input == "rollback") {
-                if (sqlite3_exec(mecro_data, "ROLLBACK;", nullptr, nullptr, &errmsg) != SQLITE_OK) {
-                    std::cerr << "Failed to rollback transaction \u25A1" << std::endl;
-                    sqlite3_free(errmsg);
-                }
-                cout << "Rolled back transaction \u25A0" << endl;
-                break;
-            }
-            std::cerr << "Unknown command \u25A1" << std::endl;
-        }
         sqlite3_close_v2(mecro_data);
     }
 }
